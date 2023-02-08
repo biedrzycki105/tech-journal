71feedf208f6ee01ca20cf554be965b61b4076ca
biedrzycki105 <90063737+biedrzycki105@users.noreply.github.com>
2023-02-07 20:07:02 -0500

    Update DCconfig.ps1

--- DCconfig.ps1
+++ DCconfig.ps1
@@ -1,7 +1,7 @@
 ### Install ADDS and create a domain
-#$domain = "erik.local"
-#Add-WindowsFeature AD-Domain-Services
-#Install-ADDSForest -DomainName $dom -InstallDNS
+$domain = "erik.local"
+Add-WindowsFeature AD-Domain-Services
+Install-ADDSForest -DomainName $domain -InstallDNS
 ### Making A Records
 Add-DnsServerResourceRecordA -Name "xubuntu-wan" -ZoneName "erik.local" -AllowUpdateAny -IPv4Address "10.0.17.100"
 Add-DnsServerResourceRecordA -Name "480-fw" -ZoneName "erik.local" -AllowUpdateAny -IPv4Address "10.0.17.2"
@@ -9,7 +9,7 @@ Add-DnsServerResourceRecordA -Name "vcenter" -ZoneName "erik.local" -AllowUpdate
 ### Create Reverse Lookup Zone
 Add-DnsServerPrimaryZone -NetworkID "10.0.17.0/24" -ReplicationScope "Domain"
 ### Making PTR Records
-Add-DnsServerResourceRecordPtr -Name "100" -ZoneName "0.17.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "xubuntu-wan.erik.local"
-Add-DnsServerResourceRecordPtr -Name "2" -ZoneName "0.17.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "480-fw.erik.local"
-Add-DnsServerResourceRecordPtr -Name "3" -ZoneName "0.17.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "vcenter.erik.local"
-Add-DnsServerResourceRecordPtr -Name "4" -ZoneName "0.17.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "dc1.erik.local"
+Add-DnsServerResourceRecordPtr -Name "100" -ZoneName "17.0.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "xubuntu-wan.erik.local"
+Add-DnsServerResourceRecordPtr -Name "2" -ZoneName "17.0.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "480-fw.erik.local"
+Add-DnsServerResourceRecordPtr -Name "3" -ZoneName "17.0.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "vcenter.erik.local"
+Add-DnsServerResourceRecordPtr -Name "4" -ZoneName "17.0.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "dc1.erik.local"
