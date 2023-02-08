022f8f03bf2aca5592edaee1d94bc0c5b4948833
biedrzycki105 <90063737+biedrzycki105@users.noreply.github.com>
2023-02-07 20:34:15 -0500

    Update DCconfig.ps1

--- DCconfig.ps1
+++ DCconfig.ps1
@@ -12,3 +12,9 @@ Add-DnsServerResourceRecordPtr -Name "100" -ZoneName "17.0.10.in-addr.arpa" -All
 Add-DnsServerResourceRecordPtr -Name "2" -ZoneName "17.0.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "480-fw.erik.local"
 Add-DnsServerResourceRecordPtr -Name "3" -ZoneName "17.0.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "vcenter.erik.local"
 Add-DnsServerResourceRecordPtr -Name "4" -ZoneName "17.0.10.in-addr.arpa" -AllowUpdateAny -PtrDomainName "dc1.erik.local"
+### Install DHCP
+Install-WindowsFeature DHCP -IncludeManagementTools
+Add-DHCPServerv4Scope -Name “480” -StartRange 10.0.17.101 -EndRange 10.0.17.150 -SubnetMask 255.255.255.0 -State Active
+Set-DHCPServerv4OptionValue -ScopeID 192.168.64.0 -DnsDomain corp.momco.com -DnsServer 192.168.64.2 -Router 192.168.64.1
+Add-DhcpServerInDC -DnsName corp.momco.com -IpAddress 192.168.64.2
+Restart-service dhcpserver
