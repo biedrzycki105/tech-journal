3a961e1f1709851f1a6172545a637b64dd7c6358
biedrzycki105 <90063737+biedrzycki105@users.noreply.github.com>
2023-02-14 23:00:14 -0500

    Add files via upload

---
+++ LinkedClones.ps1
@@ -0,0 +1,33 @@
+### Erik Biedrzycki | Cloner 
+
+### Connect to vCenter
+$vcenter = "vcenter.erik.local"
+Connect-VIServer -Server $vcenter
+
+
+### Locates VMs and lists out all VMs in datastore
+$datastore = Get-DataStore -Name "datastore1-super27"
+$allVMs = @(Get-VM -Location $datastore)
+$i = 0
+foreach($vmChoice in $allVMs)
+{
+    Write-Host [$i] $vmChoice
+    $i++
+}
+
+### Asks for VM that needs to be cloned
+$vmname = Read-Host "What VM do you want to clone? Please type full VM name as listed (i.e. xubuntu-wan)"
+$vm = Get-VM -Name $vmname
+
+### Grabs snapshot from VM
+$snapshot = Get-Snapshot -VM $vm -Name "base"
+
+### Gets ESXi Host
+$vmhost = Get-VMHost -Name "192.168.7.37"
+
+### Creates Linked Clone
+$linkedClone = "{0}.linked" -f $vm.name
+$linkedvm = New-VM -LinkedClone -Name $linkedClone -VM -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $datastore
+
+### Creates New VM from Linked Clone
+$newvm = New-VM -Name "server.2019.gui.base" -VM $linkedvm -VMHost $vmhost -Datastore $datastore
