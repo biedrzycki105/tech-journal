d40a7d83780d0c7a8c21e4ef6aaf5e84ea11c741
biedrzycki105 <erik.biedrzycki@mymail.champlain.edu>
2023-03-01 01:44:08 -0500

    480-utils update

--- 480-DevOps/modules/480-utils/480-utils.psm1
+++ 480-DevOps/modules/480-utils/480-utils.psm1
@@ -11,19 +11,45 @@ function 480Banner()
     Write-Host $banner 
 }
 
-function ListFolders()
+function SelectFolder()
 {
-
+    $allFolders = @(Get-Folder -Location $datastore)
+    $i = 0
+    foreach($folderChoice in $allFolders)
+    {
+        Write-Host [$i] $folderChoice
+        $i++
+    }
+    $folder = Read-Host "What folder do you want to clone from?"
 }
 
-function ListVMs()
+function SelectVM()
 {
-
+    $allVMs = @(Get-VM -Location $folder)
+    $i = 0
+    foreach($vmChoice in $allVMs)
+    {
+        Write-Host [$i] $vmChoice
+        $i++
+    }
+    $vm_selected = Read-Host "What VM do you want to clone? Please type full VM name as listed (i.e. xubuntu-wan)"
 }
 
 function DeployLinkedClone()
 {
+$vm = Get-VM -Name $allVMs[$vm_selected] 
 
+### Grabs snapshot from VM
+$snapshot = Get-Snapshot -VM $vm -Name $snapshot_name
+
+### Gets ESXi Host
+$vmhost = Get-VMHost -Name $esxi_host
+
+### Creates Linked Clone
+$linkedClone = Read-Host "What will the new VM be called?"
+$linkedvm = New-VM -LinkedClone -Name $linkedClone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $datastore -Location $linked_folder
+$linkedvm | New-Snapshot -Name $snapshot_name
+$linkedvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $network
 }
 
 function DeployFullClone()
