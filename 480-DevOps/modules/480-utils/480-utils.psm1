79514198dce9ef632570bafa38a424a4394613eb
biedrzycki105 <erik.biedrzycki@mymail.champlain.edu>
2023-03-01 05:35:12 -0500

    update

--- 480-DevOps/modules/480-utils/480-utils.psm1
+++ 480-DevOps/modules/480-utils/480-utils.psm1
@@ -11,48 +11,78 @@ function 480Banner()
     Write-Host $banner 
 }
 
-function SelectFolder()
+function Get-480Config([string] $config_path)
 {
-    $allFolders = @(Get-Folder -Location $datastore)
+    $conf = (Get-Content -Raw -Path $config_path | ConvertFrom-Json)
+    return $conf
+}
+
+function Connect-480Server([string] $vcenter_server)
+{
+    Connect-VIServer -Server $vcenter_server
+}
+
+function Select-Folder()
+{
+    $folders = @(Get-Folder -Type VM)
     $i = 0
-    foreach($folderChoice in $allFolders)
+    foreach($folder in $folders)
     {
-        Write-Host [$i] $folderChoice
+        Write-Host [$i] $folder
         $i++
     }
-    $folder = Read-Host "What folder do you want to clone from?"
+    $folderChoice = Read-Host "What folder do you want to clone from? Please type the index number only"
+    $folder_selected = $folders[$folderChoice]
+    return $folder_selected
 }
 
-function SelectVM()
+function Select-VM()
 {
-    $allVMs = @(Get-VM -Location $folder)
+    $folder_selected = Select-Folder
+    $vms = @(Get-VM -Location $folder_selected)
     $i = 0
-    foreach($vmChoice in $allVMs)
+    foreach($vm in $vms)
     {
-        Write-Host [$i] $vmChoice
+        Write-Host [$i] $vm
         $i++
     }
-    $vm_selected = Read-Host "What VM do you want to clone? Please type full VM name as listed (i.e. xubuntu-wan)"
+    $vmChoice = Read-Host "What VM do you want to clone? Please type the index number only"
+    $vm_selected = $vms[$vmChoice]
+    return $vm_selected
 }
 
-function DeployLinkedClone()
+function Deploy-LinkedClone()
 {
-$vm = Get-VM -Name $allVMs[$vm_selected] 
+    Get-480Config -config_path "/home/xubuntu-wan/Documents/GitHub/tech-journal/480-DevOps/configs/480.json"
+    $vm_selected = Select-VM
 
-### Grabs snapshot from VM
-$snapshot = Get-Snapshot -VM $vm -Name $snapshot_name
+    ### Grabs snapshot from VM
+    $snapshot = Get-Snapshot -VM $vm_selected -Name $conf.snapshot_name
 
-### Gets ESXi Host
-$vmhost = Get-VMHost -Name $esxi_host
+    ### Gets ESXi Host
+    $vmhost = Get-VMHost -Name $conf.esxi_host
 
-### Creates Linked Clone
-$linkedClone = Read-Host "What will the new VM be called?"
-$linkedvm = New-VM -LinkedClone -Name $linkedClone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $datastore -Location $linked_folder
-$linkedvm | New-Snapshot -Name $snapshot_name
-$linkedvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $network
+    ### Creates Linked Clone
+    $linkedClone = Read-Host "What will the new VM be called?"
+    $linkedvm = New-VM -LinkedClone -Name $linkedClone -VM $vm_selected -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $conf.datastore -Location $conf.linked_folder
+    $linkedvm | New-Snapshot -Name $conf.snapshot_name
+    $linkedvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $conf.network
 }
 
-function DeployFullClone()
+function Deploy-FullClone()
 {
+    Get-480Config -config_path "/home/xubuntu-wan/Documents/GitHub/tech-journal/480-DevOps/configs/480.json"
+    $vm_selected = Select-VM
+    $snapshot = Get-Snapshot -VM $vm_selected -Name $conf.snapshot_name
+    $vmhost = Get-VMHost -Name $conf.esxi_host
+    $linkedClone = "{0}.linked" -f $vm_selected.name
+    $linkedvm = New-VM -LinkedClone -Name $linkedClone -VM $vm_selected -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $conf.datastore -Location $conf.linked_folder
     
+    ### Creates New VM from Linked Clone and takes snapshot
+    $newvmname = Read-Host "What would you like your new Base VM to be named?"
+    $newvm = New-VM -Name $newvmname -VM $linkedvm -VMHost $vmhost -Datastore $conf.datastore -Location $conf.linked_folder
+    $newvm | New-Snapshot -Name $conf.snapshot_name
+    
+    ### Removes Linked VM (vm.linked)
+    $linkedvm | Remove-VM
 }
--- 480-DevOps/scripts/480driver.ps1
+++ 480-DevOps/scripts/480driver.ps1
@@ -1,1 +1,9 @@
-Import-Module .\480-utils
+Import-Module 480-utils -Force
+
+480Banner
+$conf = Get-480Config -config_path "/home/xubuntu-wan/Documents/GitHub/tech-journal/480-DevOps/configs/480.json"
+Connect-480Server -vcenter_server $conf.vcenter_server
+
+#Deploy-LinkedClone
+
+Deploy-FullClone
