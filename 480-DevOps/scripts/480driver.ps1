589be101963cf506d4a403fff60fcf727cc641c1
biedrzycki105 <erik.biedrzycki@mymail.champlain.edu>
2023-05-03 03:03:50 -0400

    update

--- 480-DevOps/scripts/480driver.ps1
+++ 480-DevOps/scripts/480driver.ps1
@@ -9,15 +9,15 @@ Connect-480Server -vcenter_server $conf.vcenter_server
 #windows10.ltsc.base
 #server.2019.core.base
 
-Set-Network -VMName blue1-wks1 -location "BLUE1-LAN"
-Set-Network -VMName blue1-dc1 -location "BLUE1-LAN"
+#Set-Network -VMName blue1-wks1 -location "BLUE1-LAN"
+Set-Network -VMName ubuntu1 -location "BLUE1-LAN"
 #Deploy-LinkedClone -VM "server.2019.core.base" -CloneName "blue1-dc1" -Folder "BLUE1-LAN" 
-#Deploy-LinkedClone -VM "windows10.ltsc.base" -CloneName "blue1-wks1" -Folder "BLUE1-LAN" 
+#Deploy-LinkedClone -VM "ubuntu-server.22.04.lts.base2" -CloneName "ubuntu1" -Folder "BLUE1-LAN" 
 #Deploy-FullClone
 
 #New-Network -NetworkName "Milestone6Test" -Server $conf.vcenter_server -VMHost $conf.esxi_host
-#Get-IP -VMName "ubuntu1" -VMHost $conf.esxi_host
-#Get-IP -VMName "ubuntu2" -VMHost $conf.esxi_host
+#Get-IP -VMName "rocky1" -VMHost $conf.esxi_host
+#Get-IP -VMName "rocky2" -VMHost $conf.esxi_host
 #Get-IP -VMName "rocky3" -VMHost $conf.esxi_host
 #Edit-VM -VMName "awx" -CPU "4" -MemGB "4"
 
